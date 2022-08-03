e23df193e0f8be09b47866ba2d94a7328759bcac
Jonathan Gilbert <jpg@trillica.com>
2022-08-03 21:44:12 +0100

    Now returns all measurements (or all since a point in time)

--- boditrax.js
+++ boditrax.js
@@ -1,8 +1,87 @@
 const { load } = require("cheerio");
 const request = require("request");
 
-async function main(email, password) {
+function parseBaseline(body, date) {
+  let $ = load(body);
+
+  return {
+    age: $(`#hide-printout .wheel.age`).data(),
+    height: $(`#hide-printout .wheel.height`).data(),
+    impedance: $(`#hide-printout .wheel.impedance`).data(),
+    bodyweight: $(`#hide-printout .wheel.bodyweight`).data(),
+    bmi: $(`#hide-printout .wheel.bodymassindex`).data(),
+    idealweight: $(`#hide-printout .wheel.idealweight`).data(),
+    bmr: $(`#hide-printout .wheel.basalmetabolicrate`).data(),
+    metabolicage: $(`#hide-printout .wheel.metabolicage`).data(),
+    muscleMass: {
+      weight: $(`#hide-printout .composition-material.muscle .weight`).data(
+        "value"
+      ),
+      unit: $(
+        `#hide-printout .composition-material.muscle .weight .unit`
+      ).text(),
+      percentage: $(
+        `#hide-printout .composition-material.muscle .percentage`
+      ).data("value"),
+    },
+    fatMass: {
+      weight: $(`#hide-printout .composition-material.fat .weight`).data(
+        "value"
+      ),
+      unit: $(`#hide-printout .composition-material.fat .weight .unit`).text(),
+      percentage: $(
+        `#hide-printout .composition-material.fat .percentage`
+      ).data("value"),
+    },
+    waterMass: {
+      weight: $(`#hide-printout .composition-material.water .weight`).data(
+        "value"
+      ),
+      unit: $(
+        `#hide-printout .composition-material.water .weight .unit`
+      ).text(),
+      percentage: $(
+        `#hide-printout .composition-material.water .percentage`
+      ).data("value"),
+    },
+    boneMass: {
+      weight: $(`#hide-printout .composition-material.bone .weight`).data(
+        "value"
+      ),
+      unit: $(`#hide-printout .composition-material.bone .weight .unit`).text(),
+      percentage: $(
+        `#hide-printout .composition-material.bone .percentage`
+      ).data("value"),
+    },
+    date: date * 1000,
+  };
+};
+
+
+async function main(email, password, since = new Date(new Date().setDate(new Date().getDate()-1))) {
   const jar = request.jar();
+
+  function printoutResponse(date) { 
+    return new Promise((resolve, reject) => {
+      let dateParams = date ? `&date=${date}` : "";
+    
+      request(
+        {
+          url: `https://cp.boditrax.com/dashboard/consolidated/printout?print=True${dateParams}`,
+          method: "get",
+          jar,
+        },
+        (err, res, body) => {
+          if (err) {
+            reject(err);
+            return;
+          }
+          resolve({ response: res, body });
+        }
+      );
+    })
+  };
+
   const { response, body } = await new Promise((resolve, reject) =>
     request(
       {
@@ -92,10 +171,10 @@ async function main(email, password) {
     );
   });
 
-  const printoutResponse = await new Promise((resolve, reject) => {
+  const scansResponse = await new Promise((resolve, reject) => {
     request(
       {
-        url: "https://cp.boditrax.com/dashboard/consolidated/printout?print=True",
+        url: "https://cp.boditrax.com/user/data/scans",
         method: "get",
         jar,
       },
@@ -109,86 +188,18 @@ async function main(email, password) {
     );
   });
 
-  $ = load(printoutResponse.body);
+  $ = load(scansResponse.body);
 
-  let baseline = {
-    age: $(`#hide-printout .wheel.age`).data(),
-    height: $(`#hide-printout .wheel.height`).data(),
-    impedance: $(`#hide-printout .wheel.impedance`).data(),
-    bodyweight: $(`#hide-printout .wheel.bodyweight`).data(),
-    bmi: $(`#hide-printout .wheel.bodymassindex`).data(),
-    idealweight: $(`#hide-printout .wheel.idealweight`).data(),
-    bmr: $(`#hide-printout .wheel.basalmetabolicrate`).data(),
-    metabolicage: $(`#hide-printout .wheel.metabolicage`).data(),
-    muscleMass: {
-      weight: $(`#hide-printout .composition-material.muscle .weight`).data(
-        "value"
-      ),
-      unit: $(
-        `#hide-printout .composition-material.muscle .weight .unit`
-      ).text(),
-      percentage: $(
-        `#hide-printout .composition-material.muscle .percentage`
-      ).data("value"),
-    },
-    fatMass: {
-      weight: $(`#hide-printout .composition-material.fat .weight`).data(
-        "value"
-      ),
-      unit: $(`#hide-printout .composition-material.fat .weight .unit`).text(),
-      percentage: $(
-        `#hide-printout .composition-material.fat .percentage`
-      ).data("value"),
-    },
-    waterMass: {
-      weight: $(`#hide-printout .composition-material.water .weight`).data(
-        "value"
-      ),
-      unit: $(
-        `#hide-printout .composition-material.water .weight .unit`
-      ).text(),
-      percentage: $(
-        `#hide-printout .composition-material.water .percentage`
-      ).data("value"),
-    },
-    boneMass: {
-      weight: $(`#hide-printout .composition-material.bone .weight`).data(
-        "value"
-      ),
-      unit: $(`#hide-printout .composition-material.bone .weight .unit`).text(),
-      percentage: $(
-        `#hide-printout .composition-material.bone .percentage`
-      ).data("value"),
-    },
-  };
+  const allScanDates = $("#readings > tbody > tr")
+    .map(function(){return $(this).attr("data-date");}).get();
 
-  const trackResponse = await new Promise((resolve, reject) => {
-    request(
-      {
-        url: "https://cp.boditrax.com/dashboard/track",
-        method: "get",
-        jar,
-      },
-      (err, res, body) => {
-        if (err) {
-          reject(err);
-          return;
-        }
-        resolve({ response: res, body });
-      }
-    );
-  });
+  const recentScanDates = allScanDates.filter(date => date * 1000 > since.getTime());
 
-  $ = load(trackResponse.body);
-  const times = $("#bottom script")
-    .text()
-    .replace(/\s/g, "")
-    .match(/newDate\((\d*)\)/g)
-    .map((s) => s.replace(/newDate\((\d*)\)/g, "$1"));
+  const baselines = recentScanDates.map(
+    (date) => printoutResponse(date).
+    then((data) => parseBaseline(data.body, date))
+  );
 
-  return {
-    ...baseline,
-    date: parseInt(times[times.length - 1]),
-  };
+  return await Promise.all(baselines);
 }
 module.exports = main;
